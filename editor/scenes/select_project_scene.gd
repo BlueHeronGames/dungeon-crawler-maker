37818f29c06ba57f2a3a25ac8e438b29efe2ce1d
blueheron786 <blueheron786@gmail.com>
2025-10-25 22:08:17 -0400

    Ability to load and select a project!

---
+++ editor/scenes/select_project_scene.gd
@@ -0,0 +1,108 @@
+extends Control
+
+const RECENT_PROJECTS_PATH := "user://recent_projects.cfg"
+const PROJECT_SECTION := "projects"
+const PROJECT_KEY := "paths"
+const MAX_RECENT_PROJECTS := 10
+
+@onready var project_list: ItemList = %ProjectList
+@onready var load_button: Button = %LoadButton
+@onready var file_dialog: FileDialog = %GameDataFileDialog
+@onready var notification_dialog: AcceptDialog = %NotificationDialog
+@onready var error_dialog: AcceptDialog = %ErrorDialog
+
+var recent_projects: Array[String] = []
+
+func _ready() -> void:
+	load_button.pressed.connect(_on_load_button_pressed)
+	project_list.item_activated.connect(_on_project_item_activated)
+	file_dialog.file_selected.connect(_on_file_dialog_file_selected)
+	_load_recent_projects()
+	_populate_project_list()
+
+func _load_recent_projects() -> void:
+	recent_projects.clear()
+	var config := ConfigFile.new()
+	var err := config.load(RECENT_PROJECTS_PATH)
+	if err != OK:
+		return
+	var stored_variant : Array = config.get_value(PROJECT_SECTION, PROJECT_KEY, [])
+	var stored_paths: Array = []
+	if stored_variant is Array:
+		stored_paths = stored_variant
+		for entry in stored_paths:
+			if entry is String and FileAccess.file_exists(entry):
+				recent_projects.append(entry)
+	if recent_projects.size() < stored_paths.size():
+		_save_recent_projects()
+
+func _populate_project_list() -> void:
+	project_list.clear()
+	for path in recent_projects:
+		var display_text := _format_project_display(path)
+		var index := project_list.add_item(display_text)
+		project_list.set_item_metadata(index, path)
+
+func _format_project_display(path: String) -> String:
+	var project_dir := path.get_base_dir()
+	var dir_name := project_dir.get_file()
+	if dir_name.is_empty():
+		dir_name = project_dir
+	return "%s (%s)" % [dir_name, project_dir]
+
+func _on_load_button_pressed() -> void:
+	_open_file_dialog()
+
+func _on_project_item_activated(index: int) -> void:
+	var path : Variant = project_list.get_item_metadata(index)
+	if typeof(path) == TYPE_STRING:
+		_attempt_project_load(path)
+
+func _open_file_dialog() -> void:
+	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
+	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	file_dialog.filters = PackedStringArray(["*.json ; JSON Files"])
+	file_dialog.current_file = "game_data.json"
+	file_dialog.popup_centered_ratio(0.7)
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	_attempt_project_load(path)
+
+func _attempt_project_load(path: String) -> void:
+	if !_is_valid_game_data_path(path):
+		_show_error("Please select a valid game_data.json file.")
+		return
+	if !FileAccess.file_exists(path):
+		_show_error("Selected file could not be found.")
+		return
+	_add_recent_project(path)
+	_show_success(path)
+
+func _is_valid_game_data_path(path: String) -> bool:
+	return path.get_file().to_lower() == "game_data.json"
+
+func _add_recent_project(path: String) -> void:
+	# Keep the list ordered with the most recent project at the top.
+	recent_projects.erase(path)
+	recent_projects.insert(0, path)
+	if recent_projects.size() > MAX_RECENT_PROJECTS:
+		recent_projects.resize(MAX_RECENT_PROJECTS)
+	_save_recent_projects()
+	_populate_project_list()
+
+func _save_recent_projects() -> void:
+	var config := ConfigFile.new()
+	config.set_value(PROJECT_SECTION, PROJECT_KEY, recent_projects)
+	var err := config.save(RECENT_PROJECTS_PATH)
+	if err != OK:
+		push_warning("Failed to save recent projects: %s" % err)
+
+func _show_success(path: String) -> void:
+	notification_dialog.title = "Project Loaded"
+	notification_dialog.dialog_text = "Congratulations! Loaded project from:\n%s" % path.get_base_dir()
+	notification_dialog.popup_centered()
+
+func _show_error(message: String) -> void:
+	error_dialog.title = "Unable to Load"
+	error_dialog.dialog_text = message
+	error_dialog.popup_centered()
